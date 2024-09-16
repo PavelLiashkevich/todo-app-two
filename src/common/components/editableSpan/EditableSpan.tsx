b4b0977e9188d2efaa1db562fad92eb9c8460ccf
pavel <paul8paul8@mail.ru>
2024-09-16 14:56:35 +0300

    fix: styles

--- src/common/components/editableSpan/EditableSpan.tsx
+++ src/common/components/editableSpan/EditableSpan.tsx
@@ -1,47 +1,55 @@
-import React from 'react'
-import { ChangeEvent, useState } from 'react'
-import { TextField } from '@mui/material'
+import React from "react"
+import { ChangeEvent, useState } from "react"
+import { TextField } from "@mui/material"
+import styled from "styled-components"
 
 type Props = {
-	oldTitle: string
-	isDone?: boolean
-	onChange: (title: string) => void
+  oldTitle: string
+  isDone?: boolean
+  onChange: (title: string) => void
 }
 
 export const EditableSpan = React.memo(
-	({ oldTitle, isDone, onChange }: Props) => {
-		let [editMode, setEditMode] = useState(false)
-		let [newTitle, setNewTitle] = useState('')
+  ({ oldTitle, isDone, onChange }: Props) => {
+    let [editMode, setEditMode] = useState(false)
+    let [newTitle, setNewTitle] = useState("")
 
-		const activateEditMode = () => {
-			setEditMode(true)
-			setNewTitle(oldTitle)
-		}
-		const activateViewMode = () => {
-			setEditMode(false)
-			onChange(newTitle)
-		}
+    const activateEditMode = () => {
+      setEditMode(true)
+      setNewTitle(oldTitle)
+    }
+    const activateViewMode = () => {
+      setEditMode(false)
+      onChange(newTitle)
+    }
 
-		const onChangeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
-			setNewTitle(event.currentTarget.value)
-		}
+    const onChangeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      setNewTitle(event.currentTarget.value)
+    }
 
-		return editMode ? (
-			<TextField
-				value={newTitle}
-				onChange={onChangeTitleHandler}
-				onBlur={activateViewMode}
-				className={isDone ? 'is-done' : ''}
-				variant='standard'
-				autoFocus
-			/>
-		) : (
-			<span
-				onDoubleClick={activateEditMode}
-				className={isDone ? 'is-done' : ''}
-			>
-				{oldTitle}
-			</span>
-		)
-	}
+    return editMode ? (
+      <TextField
+        value={newTitle}
+        onChange={onChangeTitleHandler}
+        onBlur={activateViewMode}
+        className={isDone ? "is-done" : ""}
+        variant="standard"
+        autoFocus
+      />
+    ) : (
+      <StyledSpan
+        onDoubleClick={activateEditMode}
+        className={isDone ? "is-done" : ""}
+      >
+        {oldTitle}
+      </StyledSpan>
+    )
+  }
 )
+
+const StyledSpan = styled.span`
+  width: 180px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`
